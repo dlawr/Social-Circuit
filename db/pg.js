f5b62ffaf8bdf22758ea7057372c7e789d3e5728
dlawr <dan.lawrence3@gmail.com>
2016-02-25 17:53:26 -0500

    required bcrypt, express-session

--- db/pg.js
+++ db/pg.js
@@ -1,1 +1,10 @@
 var pg = require('pg');
+var connectionString = "postgres://dan:wak24pie@localhost/social_circuit";
+var bcrypt = require('bcrypt');
+var salt = bcrypt.genSaltSync(10);
+var session = require('express-session');
+
+
+function createUser(req, res, next) {
+
+}
