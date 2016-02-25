45368b3cfd12afbf980a0b6c2977664c98eff626
dlawr <dan.lawrence3@gmail.com>
2016-02-25 16:36:54 -0500

    server runs

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('hit--------------/');
+});
+
+
+
+
+var port = 3000;
+var server = app.listen(port)
