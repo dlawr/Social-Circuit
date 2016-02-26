6b52aef238f3f61eb8354edd34809c3a54909dac
dlawr <dan.lawrence3@gmail.com>
2016-02-26 11:43:51 -0500

    create user function

--- db/pg.js
+++ db/pg.js
@@ -6,5 +6,26 @@ var session = require('express-session');
 
 
 function createUser(req, res, next) {
+  createSecure(req.body.email, req.body.password, saveUser);
 
+  function saveUser(email, hash) {
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+      // Handle connection errors
+      if(err) {
+        done();
+        console.log(err);
+        return res.status(500).json({ success: false, data: err});
+      }
+
+      var query = client.query("INSERT INTO users (email, password_digest) VALUES ($1, $2);",
+        [email, hash], function(err, result) {
+          done()
+          if(err) {
+            return console.error('error, running query', err);
+          }
+          next()
+      });
+    });
+  }
 }
